c60fdf4904a0efd20c5be4631b9a2a194b1427e0
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2019-11-24 22:04:44 +0300

    add hext stticker

---
+++ R/hex_sticker.R
@@ -0,0 +1,6 @@
+#
+# img_url <- system.file("icon/up-arrow.png", package="a360importer")
+# hexSticker::sticker(img_url, package="a360importer", p_size=5, s_x=1, s_y=.75, s_width=.5, s_height = .4,
+#         filename="inst/icon/a360importer.png")
+#
+#
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Upload A360 Nigeria Legacy files
 
+<img src="inst/icon/a360importer.png" height="300"/>
+
 A set up to upload A360 Nigeria legacy files to production
 
 ## Getting started
