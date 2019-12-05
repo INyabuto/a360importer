56d6442e78b2e9d7feed1b4ba6343e29ef016461
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2019-12-05 14:13:22 +0300

    specifically loads sp sheets

--- R/load_files.R
+++ R/load_files.R
@@ -42,6 +42,49 @@ load_files <- function(path = NULL){
 }
 
 
+#'Load A360 NG legacy files into R
+#'
+#'\code{load_files} loads A360 legacy files in the specified directory into R
+#'
+#'@param path System path to the directory with data
+#'@return a list of data frames
+#'@examples
+#'\dontrun{
+#'load_files(file.path(Sys.getenv("HOME"),"data")
+#'}
+load_files_sp <- function(path = NULL){
+
+  if (!is.null(path)){
+
+    files <- vector("list", length(list.files(path, pattern = "Provision.csv")))
+
+    # stop if path is empty
+    if (length(files) == 0){
+      stop("Ensure you are in the correct path or the directory has a csv file")
+    }
+
+    file_names <- list.files(path, pattern = "Provision.csv")
+
+
+    for (i in seq_along(files)) {
+
+      files[[i]] <- data.table::fread(file.path(path, file_names[i]))
+
+    }
+
+  }else{
+    stop("System path to the directory with files must be specified", call. = FALSE)
+  }
+
+
+  names(files) <- file_names
+
+  files
+
+}
+
+
+
 #'Covert excel dates to R dates
 #'
 #'\code{to_r_date} converts xlsx date number to R date
